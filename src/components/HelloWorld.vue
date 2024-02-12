f7eec62f5107f8c92c08f379f9fa36d6b2d2cbd3
bhankymyk <bankole123michael@gmail.com>
2024-02-12 13:52:20 +0100

    feat: Navbar and About me page done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,71 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Bankole Michael</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"> <font-awesome-icon :icon="['fas', 'house']" /> Home</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="#"><font-awesome-icon :icon="['fas', 'address-card']" /> About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><font-awesome-icon :icon="['fas', 'briefcase']" /> Project</a>
+        </li>
+      </ul>
+      <span class="navbar-text">
+      </span>
+      <span class="navbar-text">
+        <li class="nav-item">
+          <a class="nav-link active" href="#"><font-awesome-icon :icon="['fas', 'list-check']" /> Skills</a>
+        </li>
+      </span>
+      <span class="navbar-text">
+        <li class="nav-item">
+          <a class="nav-link active" href="#"><font-awesome-icon :icon="['fas', 'envelope']" /> Contact me</a>
+        </li>
+      </span>
+    </div>
+  </div>
+</nav>
+    <div class="row mt-5">
+      <div class="col-md text-center">
+        <h1>Hello 👋, <br>I'm <span class="inBtw">Bankole Michael</span>  <br> A Front-End Developer</h1>
+      </div>
+    </div>
+
+<div class="row">
+  <div class="col-md-8">
+    <h2 class="h-2">About Me </h2>
+    <h5 class="mt-1">I'm Bankole Michael a gradute of ALX specialized in frontend, am passionate about continous learning software and technology in general.
+      My competence in Vue empowers me to create high-performance web applications that are both user-friendly and visually captivating.</h5>
+      
+      <h5 class="mt-1"> Beyond Vue, I'm well-versed in React Js, allowing me to handle projects across multiple frameworks. I also have a strong command of 
+      UI development using CSS and Bootstrap, ensuring professional and consistent designs. </h5>
+      <h5>I'm actively seeking a full-time role as a front-end developer and would welcome the opportunity to collaborate with those who value
+         building great user interfaces and experiences. </h5>
+      <h5 class="mt-1">Outside of coding, I indulge in reading, watching football, and delving into the realms of finance and economics.</h5>
+    </div>
+    <div class="col-md-4 Contact">
+  <div> <button class="btn  btn-block btn-success">Contact Me</button>  </div>
+</div>
+</div>
+
+<!-- PROJECTS -->
+<div class="row">
+  <div class="col">
+    <h2>Projects</h2>
+  </div>
+</div>
+
+
+
   </div>
 </template>
 
@@ -41,6 +80,39 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h1{
+  font-weight: 600;
+  font-size: 34px;
+  line-height: 70px;
+}
+h5{
+  text-align: left;
+  padding: 10px;
+}
+.inBtw{
+  color: green;
+}
+h2{
+  text-align: start;
+  color: green;
+  margin-left: 20px;
+}
+.h-2::after {
+    content: "";
+    position: absolute;
+    top: 57%;
+    margin-left: 10px;
+    -webkit-transform: translateY(-50%);
+    transform: translateY(-50%);
+    width: 7rem;
+    height: 4px;
+    background-color: green;
+}
+.Contact{
+  margin-top: 200px;
+}
+
+
 h3 {
   margin: 40px 0 0;
 }
--- src/main.js
+++ src/main.js
@@ -2,4 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js"
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBriefcase, faEnvelope, faCheck, faHouse, faAddressCard, faListCheck, faTrash, faPenToSquare} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// Add the Font Awesome icons to the library
+library.add(faBriefcase, faEnvelope, faCheck, faHouse, faAddressCard, faListCheck, faTrash, faPenToSquare);
+
+// Create the Vue app instance
+const app = createApp(App);
+
+// Register the FontAwesomeIcon component globally
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+// Use the router and mount the app
+app.use(router).mount('#app');
